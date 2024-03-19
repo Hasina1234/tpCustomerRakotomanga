9cf86f356951b28995817b44f9a3d76a69c6172c
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-03-19 14:07:19 +0300

    Affichage customerList

---
+++ src/main/java/com/rakotomanga/tpcustomerrakotomanga/config/ConfigJSF.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rakotomanga.tpcustomerrakotomanga.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+/**
+ * Configuration JSF
+ * @author Hasina
+ */
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+   
+}
---
+++ src/main/java/com/rakotomanga/tpcustomerrakotomanga/jsf/CustomerBean.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.rakotomanga.tpcustomerrakotomanga.jsf;
+
+import com.rakotomanga.tpcustomerrakotomanga.entity.Customer;
+import com.rakotomanga.tpcustomerrakotomanga.service.CustomerManager;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Backing bean pour la page JSF customerList
+ * @author Hasina
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    private List<Customer> customerList;  
+
+    @Inject
+    private CustomerManager customerManager;  
+
+    public CustomerBean() {  
+        
+    }  
+
+    /** 
+     * Retourne la liste des clients pour affichage dans une DataTable.
+    */  
+    public List<Customer> getCustomers() {
+      if (customerList == null) {
+        customerList = customerManager.getAllCustomers();
+      }
+      return customerList;
+    }  
+
+}
